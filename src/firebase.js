a8bae36593509fd768269f87c43ad9b04f475261
Evan <evanm.100000@gmail.com>
2026-06-25 05:14:00 +0100

    .

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database"; 
-import { getStorage } from "firebase/storage"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your brand-new web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAL9CpDUOiNYK3ny41gK3XM6BrtrU0Nu0",
+  apiKey: "AIzaSyDAL9CpDUOinYK3ny41gK3XM6BrtrU0Nu0",
   authDomain: "oxton-staff.firebaseapp.com",
   databaseURL: "https://oxton-staff-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "oxton-staff",
   storageBucket: "oxton-staff.firebasestorage.app",
-  messagingSenderId: "88535281205",
-  appId: "1:88535281205:web:e506e4a8768e622953b43c",
+  messagingSenderId: "885352281205",
+  appId: "1:885352281205:web:e506e4a8768e622953b43c",
   measurementId: "G-PP9GD2H3F1"
 };
 
-// Initialize Firebase services cleanly (exactly once)
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-const db = getDatabase(firebaseApp);
-const storage = getStorage(firebaseApp);
-
-// Export them for your application components
-export { firebaseApp, analytics, db, storage };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
